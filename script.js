2bc4074722fa8897afc22ee5fa12e294f5239f5e
Joseph Cai <josephcai2022@gmail.com>
2023-04-30 17:13:07 -0400

    Fixed border glitch, formatted title, reordered functions, formatted code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const movideIds = [502356, 603692, 39514, 677179, 19995, 857, 405775, 4247, 315162, 12162]
+const movideIds = [857, 603692, 39514, 677179, 19995, 502356, 798286, 405775, 4247, 315162, 12162]
 
 get1.addEventListener('click', async () => {
     let num = document.getElementById("options").selectedIndex;
@@ -10,43 +10,38 @@ get1.addEventListener('click', async () => {
         }
     });
     response = response.then((moviesData) => {  
-    for (let movie of moviesData.data.results) {   
-        axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-        params: {
-            api_key: "a75766149f058c94cbde1356c3161786",
-            append_to_response: "videos",
-        }
-        }).then((extraData) => {
-            console.log(extraData)
-            if (`${movie.id}` == movideIds[num]) {
-                const img = document.getElementById('poster');
-                const iframe = document.getElementById('trailer');
-                const trailers = extraData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
-                let genre = "genre";
-
-                for (let data of extraData.data.genres) {
-                    genre = data.name;
+        for (let movie of moviesData.data.results) {   
+            axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
+                params: {
+                    api_key: "a75766149f058c94cbde1356c3161786",
+                    append_to_response: "videos",
                 }
 
-                iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
-                img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-                document.getElementById("title").innerHTML = `${movie.title}`;
-                document.getElementById("ov_label").innerHTML = "Overview";
-                document.getElementById("overview").innerHTML = `${movie.overview}`;
-                document.getElementById("basicInfo").innerHTML = `Release Date: ${movie.release_date} | Popularity: ${movie.popularity} | Runtime: ${extraData.data.runtime} | Genre: ${genre}`;
-                document.getElementById("info").innerHTML = `Tagline: ${extraData.data.tagline} | Budget: $${extraData.data.budget} | Revenue: $${extraData.data.revenue}`;
-                document.getElementById("vote").innerHTML = `Vote Average: ${movie.vote_average} | Vote Count: ${movie.vote_count}`;
-                document.getElementById("production").innerHTML = `Production Companies: ${extraData.data.production_companies[0].name}`;
+            }).then((extraData) => {
+                console.log(extraData)
 
+                if (`${movie.id}` == movideIds[num]) {
+                    const img = document.getElementById('poster');
+                    const iframe = document.getElementById('trailer');
+                    const trailers = extraData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
+                    let genre = "genre";
 
+                    for (let data of extraData.data.genres) {
+                        genre = data.name;
+                    }
 
-
-
-
-
-            }
-        });
-    }
+                    iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
+                    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+                    document.getElementById("title").innerHTML = `${movie.title}`;
+                    document.getElementById("tagline").innerHTML = `Tagline: ${extraData.data.tagline}`;  
+                    document.getElementById("overview").innerHTML = `${movie.overview}`;                    
+                    document.getElementById("basicInfo").innerHTML = `Release Date: ${movie.release_date} | Popularity: ${movie.popularity} | Runtime: ${extraData.data.runtime} | Genre: ${genre}`;
+                    document.getElementById("revenue").innerHTML = `Budget: ${extraData.data.budget} | Revenue: ${extraData.data.revenue}`;
+                    document.getElementById("production").innerHTML = `Production Companies: ${extraData.data.production_companies[0].name}`;
+                    document.getElementById("vote").innerHTML = `Vote Average: ${movie.vote_average} | Vote Count: ${movie.vote_count}`;
+                }
+            });
+        }
     });
 });
 
